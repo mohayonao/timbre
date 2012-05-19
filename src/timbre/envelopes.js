4150a552e8922165755c2b71ecea693bf2a9b2f7
mohayonao <mohayonao@gmail.com>
2012-05-19 21:21:16 +0900

    add T("tween")

--- src/timbre/envelopes.js
+++ src/timbre/envelopes.js
@@ -167,7 +167,7 @@ var ADSR = (function() {
                     continue;
                 }
                 if (mode === 3) { // S -> end
-                    mode = 4;
+                    this._mode = 4;
                     this._samples    = 0;
                     this._samplesMax = Infinity;
                     timbre.fn.do_event(this, "ended");
@@ -190,7 +190,7 @@ var ADSR = (function() {
                 break;
             case 3:
                 x = samples / samplesMax;
-                x = (1.0 - x) * s1;
+                x = (1.0 - x) * s0;
                 break;
             default:
                 x = 0;
@@ -212,6 +212,300 @@ var ADSR = (function() {
 timbre.fn.register("adsr", ADSR);
 
 
+var Tween = (function() {
+    var Tween = function() {
+        initialize.apply(this, arguments);
+    }, $this = Tween.prototype;
+    
+    Object.defineProperty($this, "type", {
+        set: function(value) {
+            var f;
+            if (typeof value === "string") {
+                if ((f = Tween.functions[value]) !== undefined) {
+                    this._type = value;
+                    this._func = f;
+                }
+            }
+        },
+        get: function() {
+            return this._type;
+        }
+    });
+    Object.defineProperty($this, "d", {
+        set: function(value) {
+            if (typeof value === "number") {
+                this._d = value;
+            }
+        },
+        get: function() {
+            return this._d;
+        }
+    });
+    Object.defineProperty($this, "start", {
+        set: function(value) {
+            if (typeof value === "number") {
+                this._start = value;
+            }
+        },
+        get: function() {
+            return this._start;
+        }
+    });
+    Object.defineProperty($this, "stop", {
+        set: function(value) {
+            if (typeof value === "number") {
+                this._stop = value;
+            }
+        },
+        get: function() {
+            return this._stop;
+        }
+    });
+    Object.defineProperty($this, "mul", {
+        set: function(value) {
+            if (typeof value === "number") {
+                this._mul = value;
+            }
+        },
+        get: function() {
+            return this._mul;
+        }
+    });
+    Object.defineProperty($this, "add", {
+        set: function(value) {
+            if (typeof value === "number") {
+                this._add = value;
+            }
+        },
+        get: function() {
+            return this._add;
+        }
+    });
+    
+    
+    var initialize = function(_args) {
+        var i, type;
+        
+        i = 0;
+        if (typeof _args[i] === "string" && (Tween.functions[_args[i]]) !== undefined) {
+            type = _args[i++];
+        } else {
+            type = "linear";
+        }
+        if (typeof _args[i] === "number") {
+            this.d = _args[i++];
+        } else {
+            this.d = 1000;
+        }
+        if (typeof _args[i] === "number") {
+            this.start = _args[i++];
+        } else {
+            this.start = 0;
+        }
+        if (typeof _args[i] === "number") {
+            this.stop = _args[i++];
+        } else {
+            this.stop = 1;
+        }
+        if (typeof _args[i] === "number") {
+            this.mul = _args[i++];
+        } else {
+            this.mul = 1;
+        }
+        if (typeof _args[i] === "number") {
+            this.add = _args[i++];
+        } else {
+            this.add = 0;
+        }
+        
+        this._phase     = 0;
+        this._phaseStep = 0;
+        this._value     = 0;
+        this._enabled   = false;
+        this.type = type;        
+    };
+    
+    $this.bang = function() {
+        var diff;
+        diff = this._stop - this._start;
+        this._phase     = 0;
+        this._phaseStep = timbre.cellsize / (this._d / 1000 * timbre.samplerate);
+        this._value     = this._func(0) * diff + this._start;
+        this._enabled   = true;
+        return this;
+    };
+    
+    $this.seq = function(seq_id) {
+        var cell;
+        var value, diff, changed, ended;
+        var i, imax;
+        
+        cell = this._cell;
+        if (seq_id !== this._seq_id) {
+            if (this._enabled) {
+                this._phase += this._phaseStep;
+                if (this._phase >= 1.0) {
+                    this._phase = 1.0;
+                    this._enabled = false;
+                    ended = true;
+                } else {
+                    ended = false;
+                }
+                diff = this._stop - this._start;
+                this._value = this._func(this._phase) * diff + this._start;
+                changed = true;
+            } else {
+                changed = false;
+            }
+            value = this._value * this._mul + this._add;
+            for (i = 0, imax = timbre.cellsize; i < imax; ++i) {
+                cell[i] = value;
+            }
+            if (changed && this.onchanged) this.onchanged(this._value);
+            if (ended) timbre.fn.do_event(this, "ended");
+        }
+        return cell;
+    };
+    
+    return Tween;
+}());
+timbre.fn.register("tween", Tween);
+
+Tween.functions = {
+    "linear": function(k) {
+        return k;
+    },
+    "quadratic.in": function(k) {
+        return k * k;
+    },
+    "quadratic.out": function(k) {
+        return k * ( 2 - k );
+    },
+    "quadratic.inout": function(k) {
+		if ( ( k *= 2 ) < 1 ) return 0.5 * k * k;
+		return - 0.5 * ( --k * ( k - 2 ) - 1 );
+    },
+    "cubic.in": function(k) {
+		return k * k * k;
+    },
+    "cubic.out": function(k) {
+        return --k * k * k + 1;
+    },
+    "cubic.inout": function(k) {
+		if ( ( k *= 2 ) < 1 ) return 0.5 * k * k * k;
+		return 0.5 * ( ( k -= 2 ) * k * k + 2 );
+    },
+    "quartic.in": function(k) {
+        return k * k * k * k;
+    },
+    "quartic.out": function(k) {
+		return 1 - --k * k * k * k;
+    },
+    "quartic.inout": function(k) {
+		if ( ( k *= 2 ) < 1) return 0.5 * k * k * k * k;
+		return - 0.5 * ( ( k -= 2 ) * k * k * k - 2 );
+    },
+    "quintic.in": function(k) {
+        return k * k * k * k * k;
+    },
+    "quintic.out": function(k) {
+		return --k * k * k * k * k + 1;
+    },
+    "quintic.inout": function(k) {
+		if ( ( k *= 2 ) < 1 ) return 0.5 * k * k * k * k * k;
+		return 0.5 * ( ( k -= 2 ) * k * k * k * k + 2 );
+    },
+    "sinusoidal.in": function(k) {
+        return 1 - Math.cos( k * Math.PI / 2 );
+    },
+    "sinusoidal.out": function(k) {
+        return Math.sin( k * Math.PI / 2 );
+    },
+    "sinusoidal.inout": function(k) {
+        return 0.5 * ( 1 - Math.cos( Math.PI * k ) );
+    },
+    "exponential.in": function(k) {
+		return k === 0 ? 0 : Math.pow( 1024, k - 1 );
+    },
+    "exponential.out": function(k) {
+		return k === 1 ? 1 : 1 - Math.pow( 2, - 10 * k );
+    },
+    "exponential.inout": function(k) {
+		if ( k === 0 ) return 0;
+		if ( k === 1 ) return 1;
+		if ( ( k *= 2 ) < 1 ) return 0.5 * Math.pow( 1024, k - 1 );
+		return 0.5 * ( - Math.pow( 2, - 10 * ( k - 1 ) ) + 2 );
+    },
+    "circular.in": function(k) {
+		return 1 - Math.sqrt( 1 - k * k );
+    },
+    "circular.out": function(k) {
+		return Math.sqrt( 1 - --k * k );
+    },
+    "circular.inout": function(k) {
+		if ( ( k *= 2 ) < 1) return - 0.5 * ( Math.sqrt( 1 - k * k) - 1);
+		return 0.5 * ( Math.sqrt( 1 - ( k -= 2) * k) + 1);
+    },
+    "elastic.in": function(k) {
+		var s, a = 0.1, p = 0.4;
+		if ( k === 0 ) return 0;
+		if ( k === 1 ) return 1;
+		if ( !a || a < 1 ) { a = 1; s = p / 4; }
+		else s = p * Math.asin( 1 / a ) / ( 2 * Math.PI );
+		return - ( a * Math.pow( 2, 10 * ( k -= 1 ) ) * Math.sin( ( k - s ) * ( 2 * Math.PI ) / p ) );
+    },
+    "elastic.out": function(k) {
+		var s, a = 0.1, p = 0.4;
+		if ( k === 0 ) return 0;
+		if ( k === 1 ) return 1;
+		if ( !a || a < 1 ) { a = 1; s = p / 4; }
+		else s = p * Math.asin( 1 / a ) / ( 2 * Math.PI );
+		return ( a * Math.pow( 2, - 10 * k) * Math.sin( ( k - s ) * ( 2 * Math.PI ) / p ) + 1 );
+    },
+    "elastic.inout": function(k) {
+		var s, a = 0.1, p = 0.4;
+		if ( k === 0 ) return 0;
+		if ( k === 1 ) return 1;
+		if ( !a || a < 1 ) { a = 1; s = p / 4; }
+		else s = p * Math.asin( 1 / a ) / ( 2 * Math.PI );
+		if ( ( k *= 2 ) < 1 ) return - 0.5 * ( a * Math.pow( 2, 10 * ( k -= 1 ) ) * Math.sin( ( k - s ) * ( 2 * Math.PI ) / p ) );
+		return a * Math.pow( 2, -10 * ( k -= 1 ) ) * Math.sin( ( k - s ) * ( 2 * Math.PI ) / p ) * 0.5 + 1;
+    },
+    "back.in": function(k) {
+		var s = 1.70158;
+		return k * k * ( ( s + 1 ) * k - s );
+    },
+    "back.out": function(k) {
+		var s = 1.70158;
+		return --k * k * ( ( s + 1 ) * k + s ) + 1;
+    },
+    "back.inout": function(k) {
+		var s = 1.70158 * 1.525;
+		if ( ( k *= 2 ) < 1 ) return 0.5 * ( k * k * ( ( s + 1 ) * k - s ) );
+		return 0.5 * ( ( k -= 2 ) * k * ( ( s + 1 ) * k + s ) + 2 );
+    },
+    "bounce.in": function(k) {
+		return 1 - Tween.functions["bounce.out"]( 1 - k );
+    },
+    "bounce.out": function(k) {
+		if ( k < ( 1 / 2.75 ) ) {
+			return 7.5625 * k * k;
+		} else if ( k < ( 2 / 2.75 ) ) {
+			return 7.5625 * ( k -= ( 1.5 / 2.75 ) ) * k + 0.75;
+		} else if ( k < ( 2.5 / 2.75 ) ) {
+			return 7.5625 * ( k -= ( 2.25 / 2.75 ) ) * k + 0.9375;
+		} else {
+			return 7.5625 * ( k -= ( 2.625 / 2.75 ) ) * k + 0.984375;
+		}
+    },
+    "bounce.inout": function(k) {
+		if ( k < 0.5 ) return Tween.functions["bounce.in"]( k * 2 ) * 0.5;
+		return Tween.functions["bounce.out"]( k * 2 - 1 ) * 0.5 + 0.5;
+    },
+};
+
+
+
 var Perc = (function() {
     var Perc = function() {
         initialize.apply(this, arguments);
@@ -227,8 +521,26 @@ var Perc = (function() {
             return this._d;
         }
     });
-    
-    var nop = function() {};
+    Object.defineProperty($this, "mul", {
+        set: function(value) {
+            if (typeof value === "number") {
+                this._mul = value;
+            }
+        },
+        get: function() {
+            return this._mul;
+        }
+    });
+    Object.defineProperty($this, "add", {
+        set: function(value) {
+            if (typeof value === "number") {
+                this._add = value;
+            }
+        },
+        get: function() {
+            return this._add;
+        }
+    });
     
     var initialize = function(_args) {
         var i;
@@ -239,13 +551,23 @@ var Perc = (function() {
         } else {
             this.d = 100.0;
         }
+        if (typeof _args[i] === "number") {
+            this.mul = _args[i++];
+        } else {
+            this.mul = 1.0;
+        }
+        if (typeof _args[i] === "number") {
+            this.add = _args[i++];
+        } else {
+            this.add = 0.0;
+        }
         if (typeof _args[i] === "function") {
             this.onended = _args[i++];
         }
         
-        this._samples = (timbre.samplerate * (this._d / 1000))|0;
+        this._samples = 0;
         this._dx = timbre.cellsize / this._samples;
-        this._x  = 1.0;
+        this._x  = 0;
     };
     
     $this.bang = function() {
@@ -257,7 +579,7 @@ var Perc = (function() {
     };
     
     $this.seq = function(seq_id) {
-        var cell;
+        var cell, val;
         var x, dx, samples;
         var i, imax;
         
@@ -266,9 +588,9 @@ var Perc = (function() {
             x  = this._x;
             dx = this._dx;
             samples = this._samples;
-            
+            val = x * this._mul + this._add;
             for (i = 0, imax = cell.length; i < imax; ++i) {
-                cell[i] = x;
+                cell[i] = val;
             }
             x -= dx;
             if (x < 0.0) x = 0.0;
