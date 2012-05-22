4e4a34b861e9819ba15996937344329cac48c0d5
mohayonao <mohayonao@gmail.com>
2012-05-23 07:05:48 +0900

    add: msec2Hz, Hz2msec, wavb

--- src/utils/converters.js
+++ src/utils/converters.js
@@ -103,9 +103,41 @@ utils.msec2bpm = function(msec, len) {
     return 60 / (msec / 1000) * (4 / len);
 };
 
+/**
+ * Convert msec to Hz
+ */
+utils.msec2Hz = function(msec) {
+    return 1000 / msec;
+};
+
+/**
+ * Convert Hz to msec
+ */
+utils.msec2Hz = function(Hz) {
+    return 1000 / Hz;
+};
+
+/**
+ * Convert string to Float32Array
+ */
+utils.wavb = function(src) {
+    var lis, i, imax, j, k, x;
+    lis = new Float32Array(1024);
+    for (i = k = 0, imax = src.length/2; i < imax; ++i) {
+        x = parseInt(src.substr(i * 2, 2), 16);
+        x = (x & 0x80) ? (x - 256) / 128.0 : x / 127.0;
+        for (j = 1024 / imax; j--; ) {
+            lis[k++] = x;
+        }
+    }
+    return lis;
+};
+
+
 utils.$exports["converter"] = [
     "mtof", "ftom", "mtoa", "ftoa", "atom", "atof",
-    "bpm2msec", "msec2bpm"
+    "bpm2msec", "msec2bpm", "msec2Hz", "msec2Hz",
+    "wavb"
 ];
 
 // __END__
