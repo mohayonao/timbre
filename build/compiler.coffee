b9d8202bb4f2e0eeca9e5da77dc0fdb1772d3896
mohayonao <mohayonao@gmail.com>
2012-05-28 16:47:43 +0900

    mv dac -> system.js

--- build/compiler.coffee
+++ build/compiler.coffee
@@ -4,8 +4,7 @@ exec = require("child_process").exec
 
 TIMBRE_SOURCES = [
     "timbre"
-    "timbre/dac"
-    "timbre/sys"
+    "timbre/system"
     "timbre/operators"
     "timbre/oscillators"
     "timbre/envelopes"
--- src/timbre/system.js
+++ src/timbre/system.js
@@ -6,6 +6,130 @@
 var timbre = require("../timbre");
 // __BEGIN__
 
+var Dac = (function() {
+    var Dac = function() {
+        initialize.apply(this, arguments);
+    }, $this = Dac.prototype;
+    
+    timbre.fn.setPrototypeOf.call($this, "ar-only");
+    
+    Object.defineProperty($this, "dac", {
+        get: function() { return this; }
+    });
+    Object.defineProperty($this, "pan", {
+        set: function(value) {
+            this._.pan = timbre(value);
+        },
+        get: function() { return this._.pan; }
+    });
+    Object.defineProperty($this, "amp", {
+        set: function(value) {
+            if (typeof value === "number") {
+                this._.mul = value;
+            }
+        },
+        get: function() { return this._.mul; }
+    });
+    
+    var initialize = function(_args) {
+        var _ = this._ = {};
+        this.args = timbre.fn.valist.call(this, _args);
+        this.pan = 0.5;
+        this.L = new Float32Array(timbre.cellsize);
+        this.R = new Float32Array(timbre.cellsize);
+        _.prev_pan = undefined;
+    };
+    
+    $this._post_init = function() {
+        var i, args;
+        args = this.args;
+        for (i = args.length; i--; ) {
+            args[i].dac = this;
+        }
+    };
+    
+    $this.clone = function(deep) {
+        var newone = timbre("dac");
+        newone._.pan = (deep) ? this._.pan.clone(true) : this._.pan;
+        timbre.fn.copy_for_clone(this, newone, deep);
+        return newone;
+    };
+
+    $this.on = function() {
+        this._.ison = true;
+        timbre.dacs.append(this);
+        timbre.fn.do_event(this, "on");
+        return this;
+    };
+    $this.off = function() {
+        this._.ison = false;
+        timbre.dacs.remove(this);
+        timbre.fn.do_event(this, "off");
+        return this;
+    };
+    $this.play = function() {
+        this._.ison = true;
+        timbre.dacs.append(this);
+        timbre.fn.do_event(this, "play");        
+        return this;
+    };
+    $this.pause = function() {
+        this._.ison = false;
+        timbre.dacs.remove(this);
+        timbre.fn.do_event(this, "pause");        
+        return this;
+    };
+    
+    $this.seq = function(seq_id) {
+        var _ = this._;
+        var args, cell, L, R;
+        var mul, pan, panL, panR;
+        var tmp, i, imax, j, jmax;
+        
+        cell = this.cell;
+        if (seq_id !== this.seq_id) {
+            args = this.args.slice(0);
+            L = this.L;
+            R = this.R;
+            pan = _.pan.seq(seq_id)[0];
+            if (pan !== _.prev_pan) {
+                _.panL = Math.cos(0.5 * Math.PI * pan);
+                _.panR = Math.sin(0.5 * Math.PI * pan);
+                _.prev_pan = pan;
+            }
+            mul  = _.mul;
+            panL = _.panL * mul;
+            panR = _.panR * mul;
+            jmax = timbre.cellsize;
+            for (j = jmax; j--; ) {
+                cell[j] = L[j] = R[j] = 0;
+            }
+            for (i = 0, imax = args.length; i < imax; ++i) {
+                if ((tmp = args[i]) !== undefined) {
+                    tmp = tmp.seq(seq_id);
+                    for (j = jmax; j--; ) {
+                        cell[j] += tmp[j] * mul;
+                        L[j] += tmp[j] * panL;
+                        R[j] += tmp[j] * panR;
+                    }
+                }
+            }
+            this.seq_id = seq_id;
+        }
+        return cell;
+    };
+    
+    return Dac;
+}());
+timbre.fn.register("dac", Dac);
+
+timbre.fn.register("pandac", Dac, function(_args) {
+    var instance = new Dac(_args.slice(1));
+    instance.pan = _args[0];
+    return instance;
+});
+
+
 var AR = (function() {
     var AR = function() {
         initialize.apply(this, arguments);
@@ -186,6 +310,9 @@ timbre.fn.register("kr", KR);
 
 // __END__
 
+describe("dac", function() {
+    object_test(Dac, "dac");
+});
 describe("ar", function() {
     object_test(AR, "ar", 0);
 });
