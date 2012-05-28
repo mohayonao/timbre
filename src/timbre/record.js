9edcb7c683dc9e9228364ad7e649dc6c862567f6
mohayonao <mohayonao@gmail.com>
2012-05-28 09:43:27 +0900

    fix: rm comment

--- src/timbre/record.js
+++ src/timbre/record.js
@@ -21,7 +21,7 @@ var Record = (function() {
             var _ = this._;
             if (typeof value === "number" && value > 0) {
                 _.recTime = value;
-                // _.buffer = new Float32Array((timbre.samplerate * _.recTime / 1000)|0);
+                _.buffer = new Float32Array((timbre.samplerate * _.recTime / 1000)|0);
             }
         },
         get: function() { return this._.recTime; }
