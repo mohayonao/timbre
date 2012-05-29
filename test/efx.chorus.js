13a687718f1cdffa0a71549d6ff4dd8d2e7953d8
mohayonao <mohayonao@gmail.com>
2012-05-29 14:14:30 +0900

    update test for efx.chorus

--- test/efx.chorus.js
+++ test/efx.chorus.js
@@ -10,35 +10,12 @@ tests = (function() {
     });
     
     tests[i] = function() {
-        return T("efx.chorus", audio);
+        return T("efx.chorus", T("saw", 1340));
     }; tests[i++].desc = "efx.chorus";
     
     tests[i] = function() {
-        var synth = T("efx.chorus", audio), toggle = false;
-        synth.onbang = function() {
-            toggle = !toggle;
-            synth.depth = (toggle) ? 0.8 : 0.4;
-        };
-        return synth;
-    }; tests[i++].desc = "efx.chorus depth";
-    
-    tests[i] = function() {
-        var synth = T("efx.chorus", audio), toggle = false;
-        synth.onbang = function() {
-            toggle = !toggle;            
-            synth.rate = (toggle) ? 1 : 0.5;
-        };
-        return synth;
-    }; tests[i++].desc = "efx.chorus rate";
-    
-    tests[i] = function() {
-        var synth = T("efx.chorus", audio), toggle = false;
-        synth.onbang = function() {
-            toggle = !toggle;
-            synth.wet = (toggle) ? 0.8 : 0.25;
-        };
-        return synth;
-    }; tests[i++].desc = "efx.chorus wet";
+        return T("efx.chorus", audio);
+    }; tests[i++].desc = "efx.chorus";
     
     return tests;
 }());
