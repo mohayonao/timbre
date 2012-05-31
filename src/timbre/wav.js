f6d4155231be219d69747bf3b0148045d279dc34
mohayonao <mohayonao@gmail.com>
2012-05-31 11:58:51 +0900

    fix

--- src/timbre/wav.js
+++ src/timbre/wav.js
@@ -8,7 +8,7 @@ var timbre = require("../timbre");
 
 /**
  * Wav: 0.1.0
- * Decode wav file
+ * Decode wav file and play it
  * [ar-only]
  */
 var Wav = (function() {
@@ -20,15 +20,31 @@ var Wav = (function() {
     
     Object.defineProperty($this, "src", {
         set: function(value) {
-            if (typeof value === "string") this._.src = value;
+            if (typeof value === "string") {
+                if (this._.src !== value) {
+                    this._.src = value;
+                    this._.isloaded = false;
+                }
+            }
         },
         get: function() { return this._.src; }
     });
     Object.defineProperty($this, "loop", {
-        set: function(value) {
-            if (typeof value === "boolean") this._.loop = value;
-        },
+        set: function(value) { this._.loop = !!value; },
         get: function() { return this._.loop; }
+    });
+    Object.defineProperty($this, "reversed", {
+        set: function(value) {
+            var _ = this._;
+            _.reversed = !!value;
+            if (_.reversed && _.phase === 0) {
+                _.phase = Math.max(0, _.buffer.length - 1);
+            }
+        },
+        get: function() { return this._.reversed; }
+    });
+    Object.defineProperty($this, "isLoaded", {
+        get: function() { return this._.isloaded; }
     });
     Object.defineProperty($this, "duration", {
         get: function() { return this._.duration; }
@@ -43,7 +59,6 @@ var Wav = (function() {
         },
         get: function() { return (this._.phase / this._.samplerate) * 1000; }
     });
-    // TODO: isLoop, isReversed
     
     var initialize = function(_args) {
         var i, _;
@@ -53,20 +68,24 @@ var Wav = (function() {
         i = 0;
         _.src  = (typeof _args[i] === "string" ) ? _args[i++] : "";
         _.loop = (typeof _args[i] === "boolean") ? _args[i++] : false;
-        
+
+        _.ison = true;
         _.loaded_src = undefined;
         _.buffer     = new Int16Array(0);
         _.samplerate = 0;
         _.duration   = 0;
         _.phaseStep  = 0;
-        _.phase = 0;
+        _.phase      = 0;
+        _.reversed   = false;
     };
     
     $this.clone = function(deep) {
         var newone, _ = this._;
         newone = timbre("wav");
-        newone._.src        = _.src;
-        newone._.loop       = _.loop;
+        newone._.src  = _.src;
+        newone._.loop = _.loop;
+        newone._.reversed = _.reversed;
+        newone._.isloaded = _.isloaded;
         newone._.loaded_src = _.loaded_src;
         newone._.buffer     = _.buffer;
         newone._.samplerate = _.samplerate;
@@ -78,7 +97,7 @@ var Wav = (function() {
     };
     
     $this.slice = function(begin, end) {
-        var newone, _ = this._, tmp;
+        var newone, _ = this._, tmp, reversed;
         if (typeof begin === "number") {
             begin = (begin / 1000) * _.samplerate;
         } else begin = 0;
@@ -89,40 +108,58 @@ var Wav = (function() {
             tmp   = begin;
             begin = end;
             end   = tmp;
+            reversed = !reversed;
         }
         newone = timbre("wav");
-        newone._.src        = _.src;
-        newone._.loop       = _.loop;
+        newone._.src  = _.src;
+        newone._.loop = _.loop;
+        newone._.reversed = _.reversed;
+        newone._.isloaded = _.isloaded;
         newone._.loaded_src = _.loaded_src;
         newone._.buffer     = _.buffer.subarray(begin, end);
         newone._.samplerate = _.samplerate;
         newone._.duration   = (end - begin / _.samplerate) * 1000;
         newone._.phaseStep  = _.phaseStep;
-        newone._.phase = 0;
+        newone._.phase = (reversed) ? Math.max(0, newone._.buffer.length - 1) : 0;
         timbre.fn.copy_for_clone(this, newone);
         return newone;
     };
     
-    var send = function(result, callback) {
-        if (typeof callback === "function") {
-            callback.call(this, result);
-        } else if (typeof callback === "object") {
-            if (result.buffer) {
-                callback.self       = this;
-                callback.samplerate = result.samplerate;
-                callback.duration   = (result.buffer.length / samplerate) * 1000;
-                callback.buffer     = result.buffer;
-                console.log("wav.load: done.");
+    var send = function(type, result, callback) {
+        if (type === "loadend") {
+            if (typeof callback === "function") {
+                callback.call(this, result);
+            } else if (typeof callback === "object") {
+                if (result.buffer) {
+                    callback.self       = this;
+                    callback.samplerate = result.samplerate;
+                    callback.duration   = (result.buffer.length / samplerate) * 1000;
+                    callback.buffer     = result.buffer;
+                    console.log("wav.load: done.");
+                }
             }
+            timbre.fn.do_event(this, "loadend", [result]);
+        } else if (type === "error") {
+            if (typeof callback === "function") {
+                callback.call(this, "error");
+            } else if (typeof callback === "object") {
+                console.log("wav.load: error.");
+            }
+            timbre.fn.do_event(this, "error");
         }
-        timbre.fn.do_event(this, "loadend", [result]);
     };
     
     $this.load = function(callback) {
         var self = this, _ = this._;
         var worker, src, m, buffer, samplerate;        
         if (_.loaded_src === _.src) {
-            send.call(this, {samplerate:_.samplerate, buffer:_.buffer}, callback);
+            if (_.samplerate === 0) {
+                send.call(this, "error", {}, callback);
+            } else {
+                send.call(this, "loadend",
+                          {samplerate:_.samplerate,
+                           buffer    :_.buffer}, callback);    
+            }
         } else if (_.src !== "") {
             timbre.fn.do_event(this, "loading");
             if (timbre.platform === "web" && timbre.workerpath) {
@@ -139,16 +176,23 @@ var Wav = (function() {
                         buffer.set(data.array, data.offset);
                         break;
                     case "ended":
+                        _.isloaded   = true;
                         _.loaded_src = _.src;
                         _.buffer     = buffer;
                         _.samplerate = samplerate;
                         _.duration   = (buffer.length / samplerate) * 1000;
                         _.phaseStep  = samplerate / timbre.samplerate;
-                        _.phase = 0;
-                        send.call(self, {samplerate:samplerate, buffer:buffer}, callback);
+                        if (_.reversed) {
+                            _.phase = Math.max(0, newone._.buffer.length - 1);
+                        } else {
+                            _.phase = 0;    
+                        }
+                        send.call(self, "loadend",
+                                  {samplerate:samplerate,
+                                   buffer    :buffer}, callback);
                         break;
                     default:
-                        send.call(self, data, callback);
+                        send.call(self, "error", {}, callback);
                         break;
                     }
                 };
@@ -163,17 +207,22 @@ var Wav = (function() {
                             _.duration   = 0;
                             _.phaseStep  = 0;
                             _.phase = 0;
-                            send.call(self, { samplerate:_.samplerate,
-                                              buffer:_.buffer }, callback);
+                            send.call(self, "error", {}, callback);
                         } else {
+                            _.isloaded   = true;
                             _.loaded_src = _.src;
                             _.buffer     = res.buffer;
                             _.samplerate = res.samplerate;
                             _.duration   = (res.buffer.length / res.samplerate) * 1000;
                             _.phaseStep  = res.samplerate / timbre.samplerate;
-                            _.phase = 0;
-                            send.call(self, { samplerate:_.samplerate,
-                                              buffer:_.buffer }, callback);
+                            if (_.reversed) {
+                                _.phase = Math.max(0, newone._.buffer.length - 1);
+                            } else {
+                                _.phase = 0;    
+                            }
+                            send.call(self, "loadend",
+                                      { samplerate:_.samplerate,
+                                        buffer    :_.buffer }, callback);
                         }
                     });
                 });
@@ -181,6 +230,7 @@ var Wav = (function() {
         } else {
             send.call(this, {}, callback);
         }
+        this._.isloaded = false;
         return this;
     };
     
@@ -196,33 +246,56 @@ var Wav = (function() {
         var buffer, phase, phaseStep;
         var index, delta, x0, x1;
         var i, imax;
+        
+        if (!_.ison) return timbre._.none;
+        
         cell = this.cell;
         if (seq_id !== this.seq_id) {
+            this.seq_id = seq_id;
             mul    = _.mul;
             add    = _.add;
             buffer = _.buffer;
-            phase  = _.phase;
             phaseStep = _.phaseStep;
-            for (i = 0, imax = cell.length; i < imax; ++i) {
-                index = phase|0;
-                delta = phase - index;
-                
-                x0 = (buffer[index    ] || 0) / 32768;
-                x1 = (buffer[index + 1] || 0) / 32768;
-                cell[i] = ((1.0 - delta) * x0 + (delta * x1)) * mul + add;
-                
-                phase += phaseStep;
-                if (buffer.length <= phase) {
-                    if (_.loop) {
-                        phase = 0;
-                        timbre.fn.do_event(this, "looped");
-                    } else {
-                        timbre.fn.do_event(this, "ended");
+            
+            if (_.reversed) {
+                for (i = 0, imax = cell.length; i < imax; ++i) {
+                    index = _.phase|0;
+                    delta = _.phase - index;
+                    
+                    x0 = (buffer[index - 1] || 0) / 32768;
+                    x1 = (buffer[index    ] || 0) / 32768;
+                    cell[i] = ((1.0 - delta) * x0 + (delta * x1)) * mul + add;
+                    
+                    _.phase -= phaseStep;
+                    if (_.phase < 0) {
+                        if (_.loop) {
+                            _.phase = Math.max(0, _.buffer.length - 1);
+                            timbre.fn.do_event(this, "looped");
+                        } else {
+                            timbre.fn.do_event(this, "ended");
+                        }
+                    }
+                }
+            } else {
+                for (i = 0, imax = cell.length; i < imax; ++i) {
+                    index = _.phase|0;
+                    delta = _.phase - index;
+                    
+                    x0 = (buffer[index    ] || 0) / 32768;
+                    x1 = (buffer[index + 1] || 0) / 32768;
+                    cell[i] = ((1.0 - delta) * x0 + (delta * x1)) * mul + add;
+                    
+                    _.phase += phaseStep;
+                    if (_.phase >= buffer.length) {
+                        if (_.loop) {
+                            _.phase = 0;
+                            timbre.fn.do_event(this, "looped");
+                        } else {
+                            timbre.fn.do_event(this, "ended");
+                        }
                     }
                 }
             }
-            _.phase = phase;
-            this.seq_id = seq_id;
         }
         return cell;
     };
