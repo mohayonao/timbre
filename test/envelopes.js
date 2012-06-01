f0fa7026c805ef40ac4bb08222ae3a48c752add0
mohayonao <mohayonao@gmail.com>
2012-06-01 14:24:20 +0900

    Update the test for PercussiveEnvelope

--- test/envelopes.js
+++ test/envelopes.js
@@ -74,8 +74,6 @@ tests = (function() {
         synth.onplay = function() { env.bang(); };
         synth.onbang = function() { env.bang(); };
         env.onended  = function() { console.log(env.currentTime); };
-        env.iteration = 30;
-        env.decayRate = 0.45;
         
         synth.$listener = T("rec", 3000).listen(env).off();
         synth.$view  = synth.$listener.buffer;
@@ -85,13 +83,30 @@ tests = (function() {
 
     tests[i] = function() {
         var synth, tri, env;
+        synth = T("*", tri = T("tri" , 1340, 0.5),
+                       env = T("perc", 500));
+        
+        synth.onplay = function() { env.bang(); };
+        synth.onbang = function() { env.bang(); };
+        env.onended  = function() { console.log(env.currentTime); };
+
+        env.delay = 1000;
+        env.a = 10;
+        env.al = 0.5;
+        
+        synth.$listener = T("rec", 3000).listen(env).off();
+        synth.$view  = synth.$listener.buffer;
+        
+        return synth;
+    }; tests[i++].desc = "perc:";
+    
+    tests[i] = function() {
+        var synth, tri, env;
         synth = T("*", T("hpf", 8000, T("noise")),
                        env = T("perc", 30), 0.1)        
         
         synth.onplay = function() { env.bang(); };
         synth.onbang = function() { env.bang(); };
-        env.iteration = 0;
-        env.decayRate = 0;
         
         synth.$listener = T("rec", 3000).listen(env).off();
         synth.$view  = synth.$listener.buffer;
