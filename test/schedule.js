e79e057e88f05b3fbf246c7f47530f15cb459e8d
mohayonao <mohayonao@gmail.com>
2012-06-14 17:59:06 +0900

    fix: schedule

--- test/schedule.js
+++ test/schedule.js
@@ -17,16 +17,21 @@ tests = (function() {
         array.onbang = function() {
             synth.args[1].bang();
         };
-        
+
         var schedule = T("schedule", "bpm(134, 16)", [
             [ 0, array], [ 2, array], [ 3, array],
             [ 4, array], [ 6, array], [ 7, array],
             [ 8, array], [10, array], [11, array],
             [12, array], [13, array], [14, array], [15, array], [16]
         ], true);
+        console.log("+++++");
         
         synth.onbang = function() {
-            schedule.bpm += 5;
+            if (schedule.getSchedules(2)[0]) {
+                schedule.remove([2, array]);
+            } else {
+                schedule.append([2, array]);
+            }
         };
         schedule.onlooped = function(count) {
             switch (count % 8) {
@@ -54,6 +59,80 @@ tests = (function() {
         
         return synth;
     }; tests[i++].desc = "schedule";
+
+    tests[i] = function() {
+        timbre.utils.exports("random.choice");
+        
+        var synth = T("+");
+        
+        function hh(vol) {
+            var tone = T("*", T("hpf", 8000, T("noise")),
+                              T("perc", 30).set("mul", vol).bang());
+            tone.args[1].onended = function() {
+                synth.remove(tone);
+            };
+            synth.append(tone);
+        }
+        function sd(vol) {
+            var tone = T("*", T("rlpf", 5000, 0.4, T("noise")),
+                              T("perc", 120).set("mul", vol).bang());
+            tone.args[1].onended = function() {
+                synth.remove(tone);
+            };
+            synth.append(tone);
+        };
+        function bd(vol) {
+            var tone = T("*", T("rlpf", 120, 0.95, T("pulse", 80), T("sin", 120, 0.8)),
+                              T("perc", 60).set("mul", vol).bang());
+            tone.args[1].onended = function() {
+                synth.remove(tone);
+            };
+            synth.append(tone);
+        };
+        
+        var hh_sch = [
+            [0, function() {
+                var x, vol;
+                if (this[0] !== this[0]|0) {
+                    x = 0.5;
+                    vol = 0.4;
+                } else {
+                    x = choice([0.5,1,1,1,1,2,2,2,4,8]);
+                    vol = 0.8;
+                }
+                this[0] += x
+                hh(vol);
+                sch.update();
+            }]
+        ];
+        var sd_sch = [
+            [4, function() {
+                sd(1);
+                this[0] += 8;
+                sch.update();
+            }]
+        ];
+        var bd_sch = [
+            [0, function() {
+                bd(1);
+                this[0] += 4;
+                sch.update();
+            }]
+        ];
+        
+        var sch = T("schedule", "bpm(120)", false);
+        sch.append(hh_sch, bd_sch, sd_sch);
+        console.log(sch._.timetable);
+        
+        synth.onplay = function() {
+            sch.on();
+        };
+        synth.onpause = function() {
+            sch.off();
+        };
+        
+        return synth;
+    }; tests[i++].desc = "schedule drumming";
     
     return tests;
 }());
