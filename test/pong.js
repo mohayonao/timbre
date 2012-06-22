c0e72fa966a071e05719dfd3b3184b0aa034e1f4
mohayonao <mohayonao@gmail.com>
2012-06-23 07:50:49 +0900

    update

--- test/pong.js
+++ test/pong.js
@@ -2,13 +2,13 @@ tests = (function() {
     "use strict";
     
     var i = 0, tests = [];
-    tests.require = ["/draft/pong.js"];
-
+    
     tests[i] = function() {
         var osc = T("sin", 880, 1.85);
         var synth = T("pong", 2, osc);
         
-        synth.$listener = T("rec", timbre.utils.hz2msec(880)*4).listen(synth).off();
+        var interval = timbre.utils.hz2msec(880)
+        synth.$listener = T("rec", interval*4, 400).listen(synth).off();
         synth.$view = synth.$listener.buffer;
         
         return synth;
