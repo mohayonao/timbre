5a5714ee57769e03eb7b556a8e6f74f4c7b5e130
mohayonao <mohayonao@gmail.com>
2012-07-08 09:15:34 +0900

    update: audio.src = [File Object]

--- src/objects/audio.js
+++ src/objects/audio.js
@@ -1,5 +1,5 @@
 /**
- * AudioDecoder: 0.3.5
+ * AudioDecoder: 0.3.7
  * Store audio samples
  * [ar-only]
  */
@@ -40,6 +40,9 @@ var AudioDecoder = {
                         this._.src = value;
                         this._.isloaded = false;
                     }
+                } else if (timbre.platform === "web" && value instanceof File) {
+                    this._.src = value;
+                    this._.isloaded = false;
                 }
             },
             get: function() { return this._.src; }
@@ -203,24 +206,16 @@ var WebKitAudio = (function() {
     $this.load = function() {
         var self = this, _ = this._;
         var src, ctx, xhr, opts;
+        var reader;
         
         ctx  = new webkitAudioContext();
         xhr  = new XMLHttpRequest();
         opts = { buffer:null, samplerate:ctx.sampleRate };
         
-        src = this.getAudioSrc(_.src);
-        if (src !== "") {
-            xhr.open("GET", src, true);
-            xhr.responseType = "arraybuffer";
-            xhr.onreadystatechange = function(event) {
-                if (xhr.readyState === 4) {
-                    if (xhr.status !== 200) {
-                        timbre.fn.doEvent(self, "error", [xhr]);
-                    }
-                }
-            };
-            xhr.onload = function() {
-                _.buffer = ctx.createBuffer(xhr.response, true).getChannelData(0);
+        if (_.src instanceof File) {
+            reader = new FileReader();
+            reader.onload = function(e) {
+                _.buffer = ctx.createBuffer(e.target.result, true).getChannelData(0);
                 _.duration  = _.buffer.length / timbre.samplerate * 1000;
                 opts.buffer = _.buffer;
                 
@@ -228,13 +223,36 @@ var WebKitAudio = (function() {
                 _.isloaded = true;
                 timbre.fn.doEvent(self, "loadeddata", [opts]);
             };
-            xhr.send();
+            reader.readAsArrayBuffer(_.src);
         } else {
-            timbre.fn.doEvent(self, "error", [xhr]);
+            src = this.getAudioSrc(_.src);
+            if (src !== "") {
+                xhr.open("GET", src, true);
+                xhr.responseType = "arraybuffer";
+                xhr.onreadystatechange = function(event) {
+                    if (xhr.readyState === 4) {
+                        if (xhr.status !== 200) {
+                            timbre.fn.doEvent(self, "error", [xhr]);
+                        }
+                    }
+                };
+                xhr.onload = function() {
+                    _.buffer = ctx.createBuffer(xhr.response, true).getChannelData(0);
+                    _.duration  = _.buffer.length / timbre.samplerate * 1000;
+                    opts.buffer = _.buffer;
+                    
+                    timbre.fn.doEvent(self, "loadedmetadata", [opts]);
+                    _.isloaded = true;
+                    timbre.fn.doEvent(self, "loadeddata", [opts]);
+                };
+                xhr.send();
+            } else {
+                timbre.fn.doEvent(self, "error", [xhr]);
+            }
+            _.isloaded = false;
+            _.buffer   = new Float32Array(0);
+            _.phase    = 0;
         }
-        _.isloaded = false;
-        _.buffer   = new Float32Array(0);
-        _.phase    = 0;
         return this;
     };
     
@@ -254,48 +272,89 @@ var MozAudio = (function() {
     }, $this = AudioDecoder.setPrototype.call(MozAudio.prototype);
     
     timbre.fn.setPrototypeOf.call($this, "ar-only");
-    
-    $this.load = function(callback) {
-        var self = this, _ = this._;
-        var src, audio, output, buffer_index, istep, opts;
-        
-        opts = { buffer:null, samplerate:0 };
 
-        src = this.getAudioSrc(_.src);
-        if (src !== "") {
-            audio = new Audio(src);
-            audio.loop = false;
-            audio.addEventListener("error", function(e) {
-                timbre.fn.doEvent(self, "error", [e]);
-            }, false);
-            audio.addEventListener("loadedmetadata", function(e) {
-                audio.volume = 0.0;
-                _.buffer = new Float32Array((audio.duration * audio.mozSampleRate)|0);
-                _.duration = audio.duration * 1000;
-                buffer_index = 0;
-                istep = audio.mozSampleRate / timbre.samplerate;
-                audio.play();
-                opts.buffer = _.buffer;
-                opts.samplerate = audio.mozSampleRate;
-                timbre.fn.doEvent(self, "loadedmetadata", [opts]);
-            }, false);
-            audio.addEventListener("MozAudioAvailable", function(e) {
-                var samples, buffer, i, imax;
+    var loadAudio = function(audio, opts) {
+        var self = this, _ = this._;
+        var output, buffer_index, istep;
+        
+        var loadFunc = function(e) {
+            var samples, buffer, i, imax;
+            try {
                 samples = e.frameBuffer;
                 buffer  = _.buffer;
                 for (i = 0, imax = samples.length; i < imax; i += istep) {
                     buffer[buffer_index++] = samples[i|0];
                 }
-            }, false);
-            audio.addEventListener("ended", function(e) {
-                _.isloaded = true;
-                timbre.fn.doEvent(self, "loadeddata", [opts]);
-            }, false);
-            audio.load();
+                audio.removeEventListener("MozAudioAvailable", loadFunc);
+                audio.addEventListener("MozAudioAvailable", function(e) {
+                    var samples, buffer, i, imax;
+                    samples = e.frameBuffer;
+                    buffer  = _.buffer;
+                    for (i = 0, imax = samples.length; i < imax; i += istep) {
+                        buffer[buffer_index++] = samples[i|0];
+                    }
+                }, false);
+            } catch (e) {
+                audio.removeEventListener("MozAudioAvailable", loadFunc);
+                audio.pause();
+                timbre.fn.doEvent(self, "error", [e]);
+            }
+        };
+        
+        audio.loop = false;
+        audio.addEventListener("error", function(e) {
+            timbre.fn.doEvent(self, "error", [e]);
+        }, false);
+        audio.addEventListener("loadedmetadata", function(e) {
+            audio.volume = 0.0;
+            _.buffer = new Float32Array((audio.duration * audio.mozSampleRate)|0);
+            _.duration = audio.duration * 1000;
+            buffer_index = 0;
+            istep = audio.mozSampleRate / timbre.samplerate;
+            audio.play();
+            opts.buffer = _.buffer;
+            opts.samplerate = audio.mozSampleRate;
+            timbre.fn.doEvent(self, "loadedmetadata", [opts]);
+        }, false);
+        audio.addEventListener("MozAudioAvailable", loadFunc, false);
+        audio.addEventListener("ended", function(e) {
+            _.isloaded = true;
+            timbre.fn.doEvent(self, "loadeddata", [opts]);
+        }, false);
+        audio.load();
+    };
+    
+    $this.load = function(callback) {
+        var self = this, _ = this._;
+        var src, reader, opts;
+        
+        opts = { buffer:null, samplerate:0 };
+        
+        if (_.src instanceof File) {
+            reader = new FileReader();
+            reader.onload = function(e) {
+                var audio, m;
+                if ((m = /^data:(.*?);/.exec(e.target.result)) !== null) {
+                    if ((new Audio("")).canPlayType(m[1])) {
+                        audio = new Audio(e.target.result);
+                        loadAudio.call(self, audio, opts);
+                    } else {
+                        timbre.fn.doEvent(self, "error", ["cannot play: '" + m[1] + "'"]);
+                    }
+                } else {
+                    timbre.fn.doEvent(self, "error", ["file error"]);
+                }
+            };
+            reader.readAsDataURL(_.src);
+        } else {        
+            src = this.getAudioSrc(_.src);
+            if (src !== "") {
+                loadAudio.call(this, new Audio(src), opts);
+            }
+            _.isloaded = false;
+            _.buffer   = new Float32Array(0);
+            _.phase    = 0;
         }
-        _.isloaded = false;
-        _.buffer   = new Float32Array(0);
-        _.phase    = 0;
         return this;
     };
     
--- test/audio.js
+++ test/audio.js
@@ -2,12 +2,13 @@ tests = (function() {
     "use strict";
     
     var i = 0, tests = [];
+    var file_audio = T("audio").set("loop", true);
     
     tests[i] = function() {
         var synth, begin;
         synth = T("audio", "/public/audio/sample.ogg", true);
         synth.onerror = function(res) {
-            console.dir("error", res);
+            console.log("error", res);
         };
         synth.onloadedmetadata = function(res) {
             begin = +new Date();
@@ -49,6 +50,40 @@ tests = (function() {
         };
         return synth;
     }; tests[i++].desc = "reversed slice";
+
+    tests[i] = function() {
+        var synth = T("buddy");
+        synth.onplay = function() {
+            synth.args[0] = file_audio.clone();
+        };
+        return synth;
+    }; tests[i++].desc = "from File (drag & drop an audio file)";
+    
+    jQuery(function() {
+        var begin;
+        file_audio.onerror = function(res) {
+            console.log("File: ERROR", res);
+        };
+        file_audio.onloadedmetadata = function(res) {
+            begin = +new Date();
+            console.log("File: loadedmetadata: buffer.length=", res.buffer.length);
+        };
+        file_audio.onloadeddata = function(res) {
+            console.log("File: loadeddata: buffer.length=", res.buffer.length);
+            console.log("File: elapsed: " + (+new Date() - begin));
+        };
+        $(document.body).on("dragover", function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+        });        
+        $(document.body).on("drop", function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            file_audio.src = e.originalEvent.dataTransfer.files[0];
+            file_audio.load();
+            console.log("file load!!");
+        });
+    });
     
     return tests;
 }());
