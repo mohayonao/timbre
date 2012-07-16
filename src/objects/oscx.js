1e8bf16cbd01aaec771e5591c59c141fc4c3a153
mohayonao <mohayonao@gmail.com>
2012-07-16 11:53:51 +0900

    update: args

--- src/objects/oscx.js
+++ src/objects/oscx.js
@@ -28,28 +28,27 @@ var PhaseOscillator = (function() {
             }
         }, // properties
         copies: [
-            "osc.wave"
+            "osc.wave", "osc.getWavetable()", "osc.setWavetable()"
         ]
     }), Oscillator = timbre.fn.getClass("osc");
     
-    
     var initialize = function(_args) {
-        var i, _;
-        
-        this._ = _ = {};
-        i = 0;
+        var _ = this._ = {};
         
         _.wave = new Float32Array(1024);
+        _.px   = 0;
+        _.fb   = 0;
+        
+        var i = 0;
+        this.wave = "sin";
         if (typeof _args[i] === "function") {
             this.wave = _args[i++];
-        } else if (typeof _args[i] === "object" && _args[i] instanceof Float32Array) {
+        } else if (_args[i] instanceof Float32Array) {
             this.wave = _args[i++];
-        } else if (typeof _args[i] === "string" && Oscillator.Wavetables[_args[i]]) {
+        } else if (typeof _args[i] === "string") {
             this.wave = _args[i++];
-        } else {
-            this.wave = "sin";
         }
-        if (typeof _args[i] !== "undefined") {
+        if (_args[i] !== undefined) {
             this.phase = _args[i++];
         } else {
             this.phase = 0;
@@ -60,13 +59,11 @@ var PhaseOscillator = (function() {
         if (typeof _args[i] === "number") {
             _.add = _args[i++];    
         }
-        _.prevx = 0;
-        _.fb    = 0;
     };
-
+    
     $this.clone = function(deep) {
-        var newone, _ = this._;
-        newone = T("oscx", _.wave);
+        var _ = this._;
+        var newone = T("oscx", _.wave);
         if (deep) {
             newone._.phase = _.phase.clone(true);
         } else {
@@ -84,45 +81,38 @@ var PhaseOscillator = (function() {
     
     $this.seq = function(seq_id) {
         var _ = this._;
-        var cell;
-        var phase, mul, add, wave;
-        var x, prevx, fb;
         var index, delta, x0, x1, xx;
         var i, imax;
         
         if (!_.ison) return timbre._.none;
         
-        cell = this.cell;
+        var cell = this.cell;
         if (seq_id !== this.seq_id) {
             this.seq_id = seq_id;
             
-            phase = _.phase.seq(seq_id);
-            fb    = _.fb;
-            mul   = _.mul;
-            add   = _.add;
-            wave  = _.wave;
-            prevx = _.prevx;
+            var phase = _.phase.seq(seq_id);
+            var fb    = _.fb;
+            var mul = _.mul, add = _.add;
+            var wave = _.wave, px = _.px;
             
-            if (_.ar && _.phase.isAr) {
+            if (_.ar && _.phase.isAr) { // ar-mode
                 for (i = 0, imax = timbre.cellsize; i < imax; ++i) {
-                    x = (phase[i] + prevx) * 1024;
-                    index = x|0;
-                    delta = x - index;
-                    x0 = wave[(index  ) & 1023];
-                    x1 = wave[(index+1) & 1023];
+                    xx = (phase[i] + px) * 1024;
+                    while (xx < 0) xx += 1024;
+                    index = xx|0; delta = xx - index;
+                    x0 = wave[index & 1023]; x1 = wave[(index+1) & 1023];
                     xx = (1.0 - delta) * x0 + delta * x1;
-                    prevx = xx * fb;
+                    px = xx * fb;
                     cell[i] = xx * mul + add;
                 }
-                _.prevx = prevx;
-            } else {
-                x = phase[0] * 1024;
-                index = x|0;
-                delta = x - index;
-                x0 = wave[(index  ) & 1023];
-                x1 = wave[(index+1) & 1023];
+                _.px = px;
+            } else {                    // kr-mode
+                xx = phase[0] * 1024;
+                while (xx < 0) xx += 1024;
+                index = xx|0; delta = xx - index;
+                x0 = wave[index & 1023]; x1 = wave[(index+1) & 1023];
                 xx = (1.0 - delta) * x0 + delta * x1;
-                for (i = 0, imax = timbre.cellsize; i < imax; ++i) {
+                for (i = imax = timbre.cellsize; i--; ) {
                     cell[i] = xx * mul + add;
                 }
             }
@@ -130,9 +120,6 @@ var PhaseOscillator = (function() {
         
         return cell;
     };
-
-    $this.getWavetable = Oscillator.getWavetable;
-    $this.setWavetable = Oscillator.setWavetable;
     
     return PhaseOscillator;
 }());
@@ -152,12 +139,6 @@ timbre.fn.register("trix", PhaseOscillator, function(_args) {
 });
 timbre.fn.register("sawx", PhaseOscillator, function(_args) {
     return new PhaseOscillator(["saw"].concat(_args));
-});
-timbre.fn.register("famix", PhaseOscillator, function(_args) {
-    return new PhaseOscillator(["fami"].concat(_args));
-});
-timbre.fn.register("konamix", PhaseOscillator, function(_args) {
-    return new PhaseOscillator(["konamix"].concat(_args));
 });
 timbre.fn.register("+sinx", PhaseOscillator, function(_args) {
     return new PhaseOscillator(["+sin"].concat(_args));
