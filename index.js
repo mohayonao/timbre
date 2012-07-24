1148fb7fbbd073536e616aee44b7d98dc3a56ce5
mohayonao <mohayonao@gmail.com>
2012-07-24 23:19:51 +0900

    update

--- index.js
+++ index.js
@@ -1,53 +1,64 @@
-ex0 = (function() {
-    return T("sin", 440);
-}());
+window.onload = function() {
+    prettyPrint();
+    
+    var cid   = -1;
+    var synth = null;
+    
+    window.play = function(id) {
+        if (cid !== id) {
+            cid = id;
+            if (synth !== null) {
+                synth.pause();
+            }
+            if (id === -1) {
+                timbre.dacs.removeAll();
+            } else {
+                synth = synthdef[id]().play();
+            }
+        }
+    };
 
-ex1 = (function() {
-    return T("+", T("sin", 523.35),
-                  T("sin", 659.25),
-                  T("sin", 783.99)).set({mul: 0.25});
-}());
-
-ex2 = (function() {
-    var synth = T("*", T("+", T("sin", 523.35),
-                              T("sin", 659.25),
-                              T("sin", 783.99)).set({mul: 0.25}),
-                       T("+tri", 8),
-                       T("adsr", 0, 1500).bang());
-    synth.onplay = synth.onbang = function() {
-        synth.args[2].bang();
-    };
-    return synth;
-}());
-
-ex3 = (function() {
-    timbre.utils.exports("atof");
-    
-    var synth = T("efx.delay", 150, 0.8);
-    
-    function tone(freq) {
-        synth.append(  T("*", T("+", T("sin", freq, 0.25), T("sin", freq + 4, 0.1)),
-                              T("perc", 2500).bang()) );
-        if (synth.args.length > 4) synth.args.shift();
-    }
-    
-    var sch = T("schedule", "bpm (116, 2)", [
-        [0, tone, [atof("G3")]],
-        [1, tone, [atof("B4")]], [1, tone, [atof("D5")]], [1, tone, [atof("F#5")]],
-        [3, tone, [atof("D3")]],
-        [4, tone, [atof("A4")]], [4, tone, [atof("C#5")]], [4, tone, [atof("F#5")]],
-        [6],
-    ], true);
-    
-    synth.onbang = function() {
-        sch.bang();
-    };
-    synth.onplay = function() {
-        sch.on();
-    };
-    synth.onpause = function() {
-        sch.off();
-    };
-    
-    return synth;
-}());
+    var synthdef = [
+        function() {
+            return T("sin", 440);
+        },
+        function() {
+            return T("+", T("sin", 523.35),
+                          T("sin", 659.25),
+                          T("sin", 783.99)).set({mul: 0.25});
+        },
+        function() {
+            return T("*", T("+", T("sin", 523.35),
+                                 T("sin", 659.25),
+                                 T("sin", 783.99)).set({mul: 0.25}),
+                          T("+tri", 8),
+                          T("perc", 15000).bang());
+        },
+        function() {
+            var mml   = T("mml", "t116 o3 $ l2 g l1 <b0<d0f+>> l2 d l1 <a0<c+0f+>>");
+            
+            mml.synth = T("efx.reverb");
+            mml.synthdef = function(freq, opts) {
+                var synth = T("*", T("+", T("tri", freq + 1, 0.25),
+                                          T("tri", freq - 1, 0.25)),
+                                   T("adsr", "24db", 100, 2500, 0.6, 1500));
+                synth.keyon = function(opts) {
+                    synth.args[1].bang();
+                };
+                synth.keyoff = function(opts) {
+                    synth.args[1].keyoff();
+                };
+                return synth;
+            };
+            
+            mml.synth.onplay = function() {
+                mml.on().bang();
+            };
+            mml.synth.onpause = function() {
+                mml.off();
+            };
+            
+            return mml.synth;
+        },
+    ];
+};
